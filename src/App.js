f72b8841c2df395f6f4dbade6fc99e63e188ef90
4j534j <4j534j@gmail.com>
2024-01-26 21:00:36 +0200

    base files

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+import Expenses from './components/Expenses/Expenses.js';
+import NewExpense from './components/NewExpense/NewExpense.js'
+
+const DUMMY_EXPEMSES = [
+    {
+      id: 'e1',
+      date: new Date(2025, 1, 18),
+      title: "New book",
+      amount: 30.99
+    },
+    {
+      id: 'e2',
+      date: new Date(2024, 10, 10),
+      title: "New jeans",
+      amount: 99.99
+    },
+     {
+      id: 'e3',
+      date: new Date(2024, 3, 10),
+      title: "New bag",
+      amount: 199.99
+    }
+  ]
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPEMSES)
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses]
+    })
+  }
+
+  return (
+    <div className="App">
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses expenses={expenses}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,121 @@
+import React, { useState, useRef, Fragment } from 'react';
+import './ExpenseForm.css'
+
+import Error from "../UI/Error"
+
+const ExpenseForm = (props) => {
+	//const [enteredTitle, setEnteredTitle] = useState('')
+	//const [enteredAmount, setEnteredAmount] = useState('')
+	//const [enteredDate, setEnteredDate] = useState('')
+
+	const [error, setError] = useState(null)
+
+	const titleInputRef = useRef()
+	const amountInputRef = useRef()
+	const dateInputRef = useRef()
+
+	/*const titleChangerHandler = (event) => {
+		setEnteredTitle(event.target.value)
+	}
+
+	const amountChangerHandler = (event) => {
+		setEnteredAmount(event.target.value)
+	}
+
+	const dateChangerHandler = (event) => {
+		setEnteredDate(event.target.value)
+	}
+	*/
+	const submitHandler = (event) => {
+		event.preventDefault()
+
+		const enteredTitle = titleInputRef.current.value
+		const enteredAmount = amountInputRef.current.value
+		const enteredDate = dateInputRef.current.value
+
+		console.log(enteredTitle)
+		console.log(enteredAmount)
+		console.log(enteredDate)
+
+		if (enteredTitle.trim().length === 0 || enteredAmount.trim().length === 0 || enteredDate.trim.length === 0) {
+			setError({
+				title: 'Invalid input',
+				message: 'Please enter a valid title or amount or date (non-empty values)'
+			})
+			return
+		}
+
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate)
+		}
+		props.onSaveExpenseData(expenseData)
+		titleInputRef.current.value = ''
+		amountInputRef.current.value = ''
+		dateInputRef.current.value = ''
+
+	}
+
+	const cancelHandler = () => {
+		props.onCancel();
+	}
+
+	const errorHandler = () => {
+		setError(null)
+	}
+	console.log(error)
+
+	return (
+		<Fragment>
+			{error && (
+				<Error
+					title={error.title}
+					message={error.message}
+					onConfirm={errorHandler}
+				/>)
+			}
+			<div>
+				<form onSubmit={submitHandler}>
+					<div className='new-expense__controls'>
+						<div className="new-expense__control">
+							<label>Title</label>
+							<input
+								type="text"
+								id="title"
+								ref={titleInputRef}
+							/>
+						</div>
+						<div className="new-expense__control">
+							<label>Amount</label>
+							<input
+								type="number"
+								min="0.01"
+								step="0.01"
+								id="amount"
+								ref={amountInputRef}
+							/>
+						</div>
+						<div className="new-expense__control">
+							<label>Date</label>
+							<input
+								type="date"
+								min="2023-01-18"
+								max="2025-12-31"
+								id="date"
+								ref={dateInputRef}
+							/>
+						</div>
+					</div>
+					<div className="new-expense__actions">
+						<button type="button" onClick={cancelHandler}>Cancel</button>
+						<button type="submit">Add Expense</button>
+					</div>
+				</form>
+			</div>
+		</Fragment>
+	)
+}
+
+
+export default ExpenseForm;
